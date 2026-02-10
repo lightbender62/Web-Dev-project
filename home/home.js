b7f4746a391a0d8a2a2ca75d2c7ce96cd0ddb0d4
Darshik <darshikladhe182@gmail.com>
2026-02-10 21:36:45 +0530

    Made Home

--- home/home.js
+++ home/home.js
@@ -2,11 +2,15 @@ const v1 = document.querySelector("#v1");
 const v2 = document.querySelector("#v2");
 const v3 = document.querySelector("#v3");
 const v4 = document.querySelector("#v4");
+const v5 = document.querySelector("#v5");
+const v6 = document.querySelector("#v6");
 
 v1.pause();
 v2.pause();
 v3.pause();
 v4.pause();
+v5.pause();
+v6.pause();
 
 const boxes = document.querySelectorAll(".box");
 
@@ -41,4 +45,20 @@ boxes[3].addEventListener("mouseenter", () => {
 
 boxes[3].addEventListener("mouseleave", () => {
   v4.pause();
+});
+boxes[4].addEventListener("mouseenter", () => {
+  v5.currentTime = 0;
+  v5.play();
+});
+
+boxes[4].addEventListener("mouseleave", () => {
+  v5.pause();
+});
+boxes[5].addEventListener("mouseenter", () => {
+  v6.currentTime = 0;
+  v6.play();
+});
+
+boxes[5].addEventListener("mouseleave", () => {
+  v6.pause();
 });
